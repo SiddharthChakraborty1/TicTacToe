15e195840f8a92d6c5723183bb846aa5722af18d
Siddharth Chakraborty <sc9921777@gmail.com>
2020-09-25 16:21:30 +0530

    Initial commit

---
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.tictactoe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    int player = 0;
+    //0 for tick, 1 for cross, 2 for empty
+    int[] gameState = {2,2,2,2,2,2,2,2,2};
+    int [][] winningPositions = {{0,1,2},{3,4,5},{6,7,8},{0,3,6},{1,4,7},{2,5,8},{0,4,8},{2,4,6}};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void dropIn(View view)
+    {
+
+        ImageView counter  = (ImageView) view;
+        if(player == 0 && gameState[Integer.parseInt((String)counter.getTag())] == 2)
+        {   gameState[Integer.parseInt((String)counter.getTag())] = 0;
+            counter.setImageResource(R.drawable.ic_baseline_done_24);
+            counter.setY(-1500);
+            counter.animate().translationY(0).rotationBy(3600).setDuration(500);
+            player = 1;
+
+        }
+        else if(player == 1 && gameState[Integer.parseInt((String)counter.getTag())] == 2)
+        {   gameState[Integer.parseInt((String) counter.getTag())] = 1;
+            counter.setImageResource(R.drawable.ic_baseline_not_interested_24);
+            counter.setY(-1500);
+            counter.animate().translationY(0).rotationBy(3600).setDuration(500);
+            player = 0;
+        }
+
+        for(int[] winningPosition : winningPositions)
+        {
+            if(gameState[winningPosition[0]] == gameState[winningPosition[1]]
+            && gameState[winningPosition[1]] == gameState[winningPosition[2]]
+            && gameState[winningPosition[0]] != 2)
+            {
+                Toast.makeText(getApplicationContext(),"someone has won",Toast.LENGTH_SHORT).show();
+            }
+        }
+
+
+
+    }
+}
